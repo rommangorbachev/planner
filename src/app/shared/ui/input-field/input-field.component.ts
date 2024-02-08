e592b5243fadfa2c05aea688a25dfc66cf8cedf7
roman.gorbachev <rn.gorbachev@gmail.com>
2024-02-08 21:31:36 +0600

    Created a custom input

---
+++ src/app/shared/ui/input-field/input-field.component.ts
@@ -0,0 +1,36 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-input-field',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './input-field.component.html',
+  styleUrls: ['./input-field.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: InputFieldComponent,
+      multi: true
+    }
+  ]
+})
+export class InputFieldComponent implements ControlValueAccessor {
+  @Input() placeholder: string = '';
+
+  value: string = '';
+  onChange: any = () => {}
+  onTouch: any = () => {}
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+  writeValue(value: string) {
+    this.value = value;
+  }
+
+}
